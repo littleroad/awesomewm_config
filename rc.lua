cc82869dde02098d580ed6ff8d844ee5b62e9ce6
Lu Fengqi <curiouslfq@gmail.com>
2020-01-21 13:05:24 +0800

    remove mylayoutbox from tray
    Signed-off-by: Lu Fengqi <curiouslfq@gmail.com>

--- rc.lua
+++ rc.lua
@@ -178,7 +178,6 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
